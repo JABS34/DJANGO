93a3554b0ba77d44824f941097bcbc86f86768d3
Jonathan Bermello <bermelloj21@gmail.com>
2025-12-27 12:00:09 -0600

    1

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -56,10 +56,10 @@ TEMPLATES = [
 WSGI_APPLICATION = "django_portfolio.wsgi.application"
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
